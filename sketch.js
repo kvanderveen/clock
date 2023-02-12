cae494add91d10bf7291b1c2b60a192445d59b90
kvanderveen <kevin.r.vanderveen@gmail.com>
2023-02-12 08:43:33 -0700

    refactored code to create a Clock class

--- sketch.js
+++ sketch.js
@@ -1,58 +1,84 @@
 function setup() {
-  createCanvas(windowWidth, windowWidth)
+  createCanvas(windowWidth, windowHeight)
   angleMode(DEGREES)
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
 function draw() {
-  noStroke()
-  background(30, 255, 255)
-  drawClockFace()
-  drawHourHand()
-  drawMinuteHand()
-  drawSecondHand()
+  const clock = new Clock()
+  clock.drawClock()
 }
 
-function drawClockFace() {
-  push()
-  translate(windowWidth / 2, windowHeight / 2)
-  fill(color('black'))
-  ellipse(0, 0, windowWidth * 0.65, windowWidth * 0.65)
-  fill(color('white'))
-  ellipse(0, 0, windowWidth * 0.6, windowWidth * 0.6)
-  fill(color('black'))
-  textSize(windowWidth * 0.05)
-  textAlign(CENTER, CENTER)
-  'III IV V VI VII VIII IX X XI XII I II'.split(' ').forEach((n, index) => {
-    text(n, windowWidth * 0.26 * cos(index * 30), windowWidth * 0.26 * sin(index * 30))
-  })
-  pop()
-}
+class Clock {
+  constructor() {
+    this.currentDateTime = new Date()
+    this.hour = (this.currentDateTime.getHours() % 12) + this.currentDateTime.getMinutes() / 60
+    this.minute = this.currentDateTime.getMinutes() + this.currentDateTime.getSeconds() / 60
+    this.second = this.currentDateTime.getSeconds() + this.currentDateTime.getMilliseconds() / 1000
+    this.degreesHour = this.hour * 30 - 90
+    this.degreesMinute = this.minute * 6 - 90
+    this.degreesSecond = this.second * 6 - 90
+    this.relativeSize = min(windowWidth, windowHeight)
+  }
 
-function drawHourHand() {
-  push()
-  translate(windowWidth / 2, windowHeight / 2)
-  fill(color('black'))
-  rotate((hour() % 12) * 30 - 90 + minute() / 2)
-  ellipse(windowWidth * 0.09, 0, windowWidth * 0.2, windowWidth * 0.02)
-  pop()
-}
+  drawClock() {
+    noStroke()
+    background(30, 255, 255)
+    this.drawClockFace()
+    this.drawHourHand()
+    this.drawMinuteHand()
+    this.drawSecondHand()
+  }
 
-function drawMinuteHand() {
-  push()
-  translate(windowWidth / 2, windowHeight / 2)
-  fill(color('black'))
-  rotate(minute() * 6 - 90 + second() / 10)
-  ellipse(windowWidth * 0.128, 0, windowWidth * 0.28, windowWidth * 0.012)
-  pop()
-}
+  drawClockFace() {
+    push()
+    translate(windowWidth / 2, windowHeight / 2)
+    fill(color('black'))
+    ellipse(0, 0, this.relativeSize * 0.65, this.relativeSize * 0.65)
+    fill(color('white'))
+    ellipse(0, 0, this.relativeSize * 0.6, this.relativeSize * 0.6)
+    fill(color('black'))
+    textSize(this.relativeSize * 0.05)
+    textAlign(CENTER, CENTER)
+    'III IV V VI VII VIII IX X XI XII I II'.split(' ').forEach((n, index) => {
+      text(
+        n,
+        this.relativeSize * 0.26 * cos(index * 30),
+        this.relativeSize * 0.26 * sin(index * 30)
+      )
+    })
+    pop()
+  }
 
-function drawSecondHand() {
-  push()
-  translate(windowWidth / 2, windowHeight / 2)
-  fill(color('red'))
-  rotate(second() * 6 - 90)
-  ellipse(windowWidth * 0.122, 0, windowWidth * 0.26, windowWidth * 0.005)
-  fill(color('black'))
-  ellipse(0, 0, windowWidth * 0.03, windowWidth * 0.03)
-  pop()
+  drawHourHand() {
+    push()
+    translate(windowWidth / 2, windowHeight / 2)
+    fill(color('black'))
+    rotate(this.degreesHour)
+    ellipse(this.relativeSize * 0.09, 0, this.relativeSize * 0.2, this.relativeSize * 0.02)
+    pop()
+  }
+
+  drawMinuteHand() {
+    push()
+    translate(windowWidth / 2, windowHeight / 2)
+    fill(color('black'))
+    rotate(this.degreesMinute)
+    ellipse(this.relativeSize * 0.128, 0, this.relativeSize * 0.28, this.relativeSize * 0.012)
+    pop()
+  }
+
+  drawSecondHand() {
+    push()
+    translate(windowWidth / 2, windowHeight / 2)
+    fill(color('red'))
+    rotate(this.degreesSecond)
+    ellipse(this.relativeSize * 0.122, 0, this.relativeSize * 0.26, this.relativeSize * 0.005)
+    fill(color('black'))
+    ellipse(0, 0, this.relativeSize * 0.03, this.relativeSize * 0.03)
+    pop()
+  }
 }
